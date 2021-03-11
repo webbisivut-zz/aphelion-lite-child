1148343adcd5c59c5722e0a317126b2161405a4e
webbisivut <info@webbisivut.org>
2021-03-11 11:05:18 +0700

    functions.php update

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@
 function theme_enqueue_styles() {
     wp_enqueue_style( 'child-frontend-style', get_stylesheet_directory_uri() . '/assets/css/frontend.min.css' );
 }
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 100 );
 
 /**
  * Languages
@@ -22,7 +22,7 @@ function aphelion_lang_setup() {
 	$lang = get_stylesheet_directory() . '/languages';
 	load_child_theme_textdomain( 'aphelion', $lang );
 }
-add_action( 'after_setup_theme', 'aphelion_lang_setup' );
+add_action( 'after_setup_theme', 'aphelion_lang_setup', 100 );
 
 /**
  * Constants. See parent themes /inc/theme-constants.php file
