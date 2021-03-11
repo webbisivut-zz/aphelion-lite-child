8d868003569bd22dca03b6ba0362a1808cb7b230
webbisivut <info@webbisivut.org>
2021-03-11 11:02:40 +0700

    Functions.php updated

--- functions.php
+++ functions.php
@@ -1,13 +1,32 @@
 <?php
+/**
+ * Aphelion Lite Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Aphelion_Lite
+ */
 
+/**
+ * Enqueue scripts and styles.
+ */
 function theme_enqueue_styles() {
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'aphelion-stylesheet' ) );
-    wp_enqueue_style( 'child-frontend-style', get_stylesheet_directory_uri() . '/assets/css/frontend.min.css', array( 'aphelion-stylesheet' ) );
+    wp_enqueue_style( 'child-frontend-style', get_stylesheet_directory_uri() . '/assets/css/frontend.min.css' );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
+/**
+ * Languages
+ */
 function aphelion_lang_setup() {
 	$lang = get_stylesheet_directory() . '/languages';
 	load_child_theme_textdomain( 'aphelion', $lang );
 }
 add_action( 'after_setup_theme', 'aphelion_lang_setup' );
+
+/**
+ * Constants. See parent themes /inc/theme-constants.php file
+ */
+if ( ! defined( '_SHOW_TOP_HEADER' ) ) {
+	define( '_SHOW_TOP_HEADER', true );
+}
