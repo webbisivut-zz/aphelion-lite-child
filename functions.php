2ec5eeaaaf2f581fb904e0235085388df0d0152d
webbisivut <info@webbisivut.org>
2023-02-15 19:01:56 +0800

    WooCommerce support

--- functions.php
+++ functions.php
@@ -30,3 +30,11 @@ add_action( 'after_setup_theme', 'aphelion_lang_setup', 100 );
 if ( ! defined( '_SHOW_TOP_HEADER' ) ) {
 	define( '_SHOW_TOP_HEADER', true );
 }
+
+/**
+ * WooCommerce support
+ */
+function aphelion_add_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
+add_action( 'after_setup_theme', 'aphelion_add_woocommerce_support' );
